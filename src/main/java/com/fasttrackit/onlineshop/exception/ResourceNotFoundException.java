7c7d672dac2cf1ae7afad67d8cfdde559a767ba2
CsMPeter <csurkampeter@gmail.com>
2019-12-02 19:58:12 +0200

    custom queries

--- src/main/java/com/fasttrackit/onlineshop/exception/ResourceNotFoundException.java
+++ src/main/java/com/fasttrackit/onlineshop/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.fasttrackit.onlineshop.exception;
 
 
-public class ResourceNotFoundException extends Exception{
+public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/fasttrackit/onlineshop/persistance/ProductRepository.java
+++ src/main/java/com/fasttrackit/onlineshop/persistance/ProductRepository.java
@@ -4,12 +4,18 @@ import com.fasttrackit.onlineshop.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     Page<Product> findByNameContaining(String partialName, Pageable pageable);
 
     Page<Product> finByNameContainingAndQuantityGreaterThanEqual(String partialName, int minQuantity, Pageable pageable);
+
+//    JPQL syntax (java persistence query language)
+//    @Query("SELECT product FROM Product product WHERE name LIKE '%:partialName%'")
+    @Query(value = "SELECT * FROM product where `name` LIKE '%?0%'", nativeQuery = true)
+    Page<Product> findByPartialName(String partialName,Pageable pageable);
 
 
 }
